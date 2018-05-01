23d8f235a8fd5511fd4dbc3d30acb7b2930bd28b
anonygrits <anonygrits@gmail.com>
2018-05-01 05:06:35 -0500

    coded parseSandwichJson

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,37 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        if (json.equals("")) {
+            throw new JSONException("no data available");
+        }
+
+        JSONObject sandwichJSON = new JSONObject(json);
+
+        /* get embedded arrays and convert to list of strings */
+        JSONArray akaJSONArray  = new JSONArray(sandwichJSON.optJSONArray("alsoKnownAs"));
+        List<String> parsedAkaList = Arrays.asList(akaJSONArray.toString());
+
+        JSONArray ingredientsJSONArray  = new JSONArray(sandwichJSON.optJSONArray("ingredients"));
+        List<String> parsedIngredientsList = Arrays.asList(ingredientsJSONArray.toString());
+
+        /* create and populate Sandwich object */
+        Sandwich sandwich = new Sandwich();
+        sandwich.setMainName(sandwichJSON.optString("mainName"));
+        sandwich.setAlsoKnownAs(parsedAkaList);
+        sandwich.setDescription(sandwichJSON.optString("description"));
+        sandwich.setIngredients(parsedIngredientsList);
+        sandwich.setPlaceOfOrigin(sandwichJSON.optString("placeOfOrigin"));
+        sandwich.setImage(sandwichJSON.optString("image"));
+        return sandwich;
     }
 }
